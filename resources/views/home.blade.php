d16af768315ce29b5d946876683ef4a8a4fde8ee
Nurfajriati <fajriati@mhs.unsyiah.ac.id>
2022-05-10 04:05:56 +0700

    Halaman home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>NoteChara</title>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+  <!-- <style type="text/css">
+    @import "style.css";
+</style> -->
+</head>
+<body>
+<nav class="navbar navbar-expand-sm navbar-dark fixed-top" style="background-color: #5f4f34;">
+  <div class="container fluid">
+  <!-- Brand -->
+  <span class="navbar-brand mb-0 h1">NoteChara</span>
+  <!-- Links -->
+</div>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+  <span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="collapsibleNavbar">
+  <ul class="navbar-nav ml-auto">
+    <li class="nav-item">
+      <a class="nav-link" href="#" ><b>Home</b></a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#"><b>Sinopsis</b></a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#"><b>Character</b></a>
+      </li>
+    <li class ="nav-item">
+        <a class="nav-link" href="#"><b>Done</b><a>
+  </ul>
+  </div>
+</nav>
+<br><br>
+<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://ae01.alicdn.com/kf/HTB1lwnDvQCWBuNjy0Faq6xUlXXaQ/Rumah-Kolam-DIY-Lukisan-dengan-Angka-Pemandangan-Vintage-Dinding-Lukisan-Akrilik-Cat-Di-Atas-Kanvas-untuk.jpg"  width="100%" height="700">
+      <div class="carousel-caption d-none d-md-block">
+        <h5><b>SMART<b></h5>
+        <p><b><i>Study more and you will be the number one!<i><b></p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="https://p4.wallpaperbetter.com/wallpaper/770/981/90/fantasy-lscape-2-green-forest-painting-wallpaper-preview.jpg" width="100%" height="700">
+      <div class="carousel-caption d-none d-md-block">
+        <h5><b>IMAGINATION<b></h5>
+        <p><b><i>Your imagination is your fantasy!<b><i></p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="https://w7.pngwing.com/pngs/413/139/png-transparent-books-background-books-retro-library.png" width="100%" height="700">
+      <div class="carousel-caption d-none d-md-block">
+        <h5><b>WRITE<b></h5>
+        <p><b><i>Write it just like you make your own world!<b><i></p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
